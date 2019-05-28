55163c5c02bfa18d63700a3824bc9007959351ad
Hung Tang <26128997+hung-tang@users.noreply.github.com>
2019-05-28 19:58:03 -0400

    sliding max

---
+++ 239.sliding-window-maximum.go
@@ -0,0 +1,44 @@
+package main
+
+/*
+ * @lc app=leetcode id=239 lang=golang
+ *
+ * [239] Sliding Window Maximum
+ */
+func maxSlidingWindow(nums []int, k int) []int {
+	res := make([]int, 0)
+	if nums == nil || len(nums) == 0 || k == 0 {
+		return res
+	}
+	q := make([]int, 0)
+	m := make([]int, 0)
+	for i := 0; i < k; i++ {
+		insert(&q, &m, k, nums[i])
+	}
+	res = append(res, m[0])
+	for i := k; i < len(nums); i++ {
+		insert(&q, &m, k, nums[i])
+		res = append(res, m[0])
+	}
+	return res
+}
+
+func insert(q *[]int, m *[]int, k int, num int) {
+	if len(*q) == k {
+		if (*m)[0] == (*q)[0] {
+			*m = (*m)[1:] // remove max
+		}
+		*q = (*q)[1:]
+	}
+	*q = append(*q, num)
+	for len(*m) > 0 && (*m)[len(*m)-1] < num {
+		*m = (*m)[:len(*m)-1]
+	}
+	*m = append(*m, num)
+}
+
+// func main() {
+// 	var nums = []int{1, 3, -1, -3, 5, 3, 6, 7}
+// 	k := 3
+// 	fmt.Println(maxSlidingWindow(nums, k))
+// }
