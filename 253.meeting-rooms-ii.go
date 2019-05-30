8e14a65e2a8f46563b11b3a86e9d34fc04c5a7dd
Hung Tang <26128997+hung-tang@users.noreply.github.com>
2019-05-30 19:46:55 -0400

    simplify

--- 253.meeting-rooms-ii.go
+++ 253.meeting-rooms-ii.go
@@ -11,24 +11,18 @@ import (
  * [253] Meeting Rooms II
  */
 func minMeetingRooms(intervals [][]int) int {
-	f := make([][]int, 0)
-	s := make([][]int, 0)
+	f, s := make([]int, len(intervals)), make([]int, len(intervals))
 	for i := range intervals {
-		f = append(f, intervals[i])
-		s = append(s, intervals[i])
+		f[i], s[i] = intervals[i][0], intervals[i][1]
 	}
-	sort.Slice(f, func(i, j int) bool {
-		return f[i][0] < f[j][0]
-	})
-	sort.Slice(s, func(i, j int) bool {
-		return s[i][1] < s[j][1]
-	})
+	sort.Ints(f)
+	sort.Ints(s)
 	res := 0
 	count := 0
 	i := 0
 	j := 0
 	for i < len(f) && j < len(s) {
-		for j < len(s) && s[j][1] <= f[i][0] {
+		for j < len(s) && s[j] <= f[i] {
 			j++
 			count--
 		}
